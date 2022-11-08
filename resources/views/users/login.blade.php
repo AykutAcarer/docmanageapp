a1e8d1604fa178d3212c17c7a109f5410b87275c
Aykut Acarer <acareraykut@gmail.com>
2022-11-08 20:37:54 +0100

    updated

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -47,7 +47,7 @@
                             <a href="#" class="nav-link">Documentation</a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">About</a>
+                            <a href="#" class="nav-link">asdasdasd</a>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link">Contact</a>
